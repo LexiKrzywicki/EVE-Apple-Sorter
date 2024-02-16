088501ff71b99cd792439630611c2b6b7444819a
Lexi Krzywicki <lexikrzywicki@yahoo.com>
2024-02-16 18:21:08 -0500

    OpenCV working with pytorch

--- src/capture.py
+++ src/capture.py
@@ -6,3 +6,8 @@ if not cap.isOpened():
     print("ERROR! Unable to open camera")
 
 result, image = cap.read()
+
+cropped = image[110:380, 0:630]
+
+# cv2.imshow('Image from Camera', cropped)
+# cv2.waitKey(0)
---
+++ src/main.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import time
+import subprocess
+import os
+import sys
+import shutil
+
+import appleGrade
+import inference
+import capture
+
+
+#if apple detected by ultrasonic take picture of apple
+#run ML first
+#run OpenCV next
+
+time_start = int(time.time() * 1000)
+
+#if no surface defect, then G1 apple
+# if len(inference.pred_classes) > 0:
+#     print("Apple is G2")
+# else:
+#     print("NOW RUNNING OPENCV")
+image = capture.cropped
+apple = appleGrade.AppleInfo(image, image, image, image, image, image, image)
+apple.orig_image = image
+
+apple.hsv = cv2.cvtColor(apple.orig_image, cv2.COLOR_BGR2HSV)
+
+apple.no_back_image = apple.remove_background()
+
+apple.red_image = apple.get_red()
+apple.get_color_percent()
+print("Percent Red = ", apple.percent_red)
+
+apple.apple_shape = apple.get_shape()
+print("GRADE:", apple.grade())
+
+time_end = int(time.time() * 1000)
+
+time_total = time_end - time_start
+print("Time(s) =", time_total)
+
+cv2.imshow("Original", apple.no_back_image)
+cv2.waitKey(0)
+print("OPENCV COMPLETE")
+
+
+    
